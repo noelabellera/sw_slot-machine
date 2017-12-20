f4ceac4f3363a59a5fc63e70d15f1de159904680
Noel Abellera <noel@Noels-MacBook-Pro.local>
2017-12-20 10:11:04 -0800

    create DOM message

--- js/main.js
+++ js/main.js
@@ -25,6 +25,7 @@ var pics = ['https://i.imgur.com/2hkRWJz.jpg', //Vader Pic
 
 /*----- cached element preferences  ------*/
 var msgEl = document.querySelector('h1');
+var subMsgEl = document.querySelector('h2');
 var betConsole = document.getElementById('showBet');
 var balanceConsole = document.getElementById('showBalance');
 var winConsole = document.getElementById('showWin');
@@ -70,7 +71,6 @@ function spinReel(){
         userBalance = userBalance - betTotal;
         checkForWinner();
         render();
-        
         }
 }
 
@@ -107,23 +107,28 @@ function renderMsg() {
     betConsole.innerHTML = "$ " + betTotal.toFixed(2);
     balanceConsole.innerHTML = "$ " + userBalance.toFixed(2);
     winConsole.innerHTML = "$ " + winVal.toFixed(2); 
-    msgEl.innerHTML = "Buying In $ " + amtBuyIn.toFixed(2);
-    if (userBalance < betTotal || userBalance === 0) {
-        msgEl.innerHTML = "Please Buy-In & Place Your Bets";
-    } else if(betTotal === 0) {
-        msgEl.innerHTML = "Please Place Your Bets";
-    } else {
+    subMsgEl.innerHTML = "Balance $ " + userBalance.toFixed(2);
+    if (userBalance < betTotal || userBalance === 0 || betTotal === 0) {
+        msgEl.innerHTML = "Place Your Bets & May The Force Be With You!";
+    } else if(winVal > 0){
+        msgEl.innerHTML = 'You Win $ ' + winVal.toFixed(2);
+    } else if(userBalance === 0) {
+        subMsgEl.innerHTML = "&nbsp";
+    }
+    else {
         msgEl.innerHTML = "Current Bet: $ " + betTotal.toFixed(2);
     }
 }
 
 function cashingOut() {
+    betTotal = 0;
+    winVal = 0
+    subMsgEl.innerHTML = "Balance $ 0.00"
     msgEl.innerHTML = "You're cashing out: $ " + userBalance.toFixed(2);
     balanceConsole.innerHTML = "$ 0.00";
+    winConsole.innerHTML = "$ " + winVal.toFixed(2);
+    betConsole.innerHTML = "$ " + betTotal.toFixed(2);
     init();
-    
-    //render();
-    
 }
 
 function init () {
@@ -134,8 +139,7 @@ function init () {
     amtBuyIn = 0;
 }
 
-function payOut(multiplier, string) {
-    console.log(string)
+function payOut(multiplier) {
     winVal = betTotal * multiplier;
     userBalance = userBalance + winVal;
 
@@ -144,27 +148,29 @@ function payOut(multiplier, string) {
 function checkForWinner(){
     if((reelResults[0] === 0) && 
         (reelResults[0] === reelResults[1]) && (reelResults[0] === reelResults[2])) {
-        payOut(100, 'You Win Vader')
+        payOut(100)
     } else if((reelResults[0] === 1) && 
         (reelResults[0] === reelResults[1]) && (reelResults[0] === reelResults[2])) {
-        payOut(75, 'You Win Yoda')
+        payOut(75)
     } else if((reelResults[0] === 2) && 
         (reelResults[0] === reelResults[1]) && (reelResults[0] === reelResults[2])) {
-        payOut(50, 'You Win R2')
+        payOut(50)
     } else if((reelResults[0] === 3) && 
         (reelResults[0] === reelResults[1]) && (reelResults[0] === reelResults[2])) {
-        payOut(25, 'You Win BB8')
+        payOut(25)
     } else if((reelResults[0] === 4) && 
         (reelResults[0] === reelResults[1]) && (reelResults[0] === reelResults[2])) {
-        payOut(15, 'You Win Chewy')
+        payOut(15)
     } else if((reelResults[0] === 5) && 
         (reelResults[0] === reelResults[1]) && (reelResults[0] === reelResults[2])) {
-        payOut(10, 'You Win The Falcon')
+        payOut(10)
     } else if((reelResults[0] === 6) && 
         (reelResults[0] === reelResults[1]) && (reelResults[0] === reelResults[2])) {
-        payOut(5, 'Boba Fett')
+        payOut(5)
     } else if (reelResults[0] === reelResults[1]) {
-        payOut(2, "May the force be with you")
+        payOut(2)
+    } else {
+        winVal = 0;
     }
     render();
 }
